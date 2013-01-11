a4c9ea22eda86231a77300924ef3d852d39f1158
Garrick Cheung <garrick@garrickcheung.com>
2013-01-11 11:10:06 -0800

    Adding ignored files/folders and updating package.json with buster devDep.

---
+++ test/buster.js
@@ -0,0 +1,11 @@
+var config = module.exports;
+
+config["Parsed"] = {
+    rootPath: "../",
+    environment: "node", // or "browser"
+    sources: [
+    ],
+    tests: [
+        "test/*-test.js"
+    ]
+};
