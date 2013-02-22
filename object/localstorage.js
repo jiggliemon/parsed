7964460135b1b2d8b4d72e2a4357da43b23dbdd5
Garrick Cheung <garrick@garrickcheung.com>
2013-02-21 17:40:32 -0800

    Local storage plugin.

---
+++ object/localstorage.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Check for window or global
+var win;
+if (typeof window != 'undefined') {
+    win = window
+} else {
+    win = global
+}
+
+var store = win.localStorage;
+
+/**
+ * LocalStorage plugin for parsed/object
+ */
+function LS(){}
+
+LS.prototype = {
+    listeners: {
+        'saved': 'save'
+    }
+
+    , save: function(key, data){
+        store && store.setItem(key, JSON.stringify(data));
+        return this;
+    }
+
+    , fetch: function(key){
+        return store && JSON.parse(store.getItem(key));
+    }
+}
+
+module.exports = LS
